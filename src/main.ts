82f87c985d1afba3ab1a29c1a4e4fcdafa16f833
Tom <tom.weelborg@gmail.com>
2025-11-24 19:42:21 +0100

    feat: add Google OAuth 2.0 login plugin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vue3GoogleLogin, {
+  clientId: import.meta.env.VITE_GOOGLE_OAUTH2_CLIENT_ID,
+})
 
 app.mount('#app')
