2eaf5b8c75e2f4962f25e720db52fe0809bbdd9c
Tom <tom.weelborg@gmail.com>
2025-11-24 01:12:40 +0100

    feat: add HomeScreen component

---
+++ src/components/HomeScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <FamilyTree :people="people" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import type { Person } from '@/types/Person'
+import FamilyTree from './FamilyTree.vue'
+
+const people = ref<Person[]>([])
+
+onMounted(() => {
+  fetch(`${import.meta.env.VITE_API_URL}/person`)
+    .then((res) => res.json())
+    .then((p: Person[]) => {
+      people.value = p
+    })
+})
+</script>
+
+<style scoped>
+div {
+  height: 100%;
+}
+</style>
