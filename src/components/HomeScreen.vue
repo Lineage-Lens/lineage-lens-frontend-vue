498f95707053c8f6beec4dc1ef3023102ea6918b
Tom <tom.weelborg@gmail.com>
2025-11-24 19:48:06 +0100

    feat: add access token to API request

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -8,11 +8,16 @@
 import { onMounted, ref } from 'vue'
 import type { Person } from '@/types/Person'
 import FamilyTree from './FamilyTree.vue'
+import auth from '../auth.ts'
 
 const people = ref<Person[]>([])
 
 onMounted(() => {
-  fetch(`${import.meta.env.VITE_API_URL}/person`)
+  fetch(`${import.meta.env.VITE_API_URL}/person`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem(auth.accessTokenLocalStorageKey)}`,
+    },
+  })
     .then((res) => res.json())
     .then((p: Person[]) => {
       people.value = p
